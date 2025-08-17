9f9832430beefd3e0b09a2fd187580402330db5d
Akhilesh Thite <akhithite@gmail.com>
2025-08-16 20:59:35 -0700

    chore: add white background to directoryListingTemplate

--- src/protocols/helia/directoryListingTemplate.js
+++ src/protocols/helia/directoryListingTemplate.js
@@ -1,6 +1,6 @@
 export const directoryListingHtml = (shortCID, filesHtml) => `
         <html>
-          <style>html, body { margin: auto; height: 100%; width: 100%; }</style>
+          <style>html, body { margin: auto; height: 100%; width: 100%; background-color: #fff }</style>
           <body>
             <div style="background-color: #22d3ee; margin: auto; width: 100%; padding: 10px;">
               <img src="peersky://static/assets/logo.png" width="50" alt="Peersky Browser Logo" />
