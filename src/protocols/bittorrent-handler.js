124f232c2d880fc899f05dbba34e7d390a440ed9
PrasannaPal21 <prasannapal273@gmail.com>
2026-04-27 15:23:30 +0530

    security(bt-api): require mutation token via X-BT-Token header only

--- src/protocols/bittorrent-handler.js
+++ src/protocols/bittorrent-handler.js
@@ -392,7 +392,7 @@ export function setupBittorrentIpc() {
 async function handleAPI(api, queryParams, infoHash, request) {
   const rawHash = queryParams.get("hash") || infoHash;
   const hash = await normalizeInfoHash(rawHash);
-  const token = request.headers?.get("x-bt-token") || queryParams.get("token");
+  const token = request.headers?.get("x-bt-token");
   log.info(`[BT] API call: ${api}, hash: ${hash}`);
 
   // Security: validate request is from BitTorrent protocol
